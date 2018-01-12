e9eaf4ff3fb694425708e0a7182a32b10864c7c7
dzimnitsky <dzimnitsky@dev-team.com>
2018-01-12 16:17:10 +0300

    integrated webpack in to a project. Add some new postcss plugins

---
+++ assets/index.js
@@ -0,0 +1,7 @@
+import './scss/main.scss';
+
+import { sup } from './js/module';
+import { hello } from './js/hello';
+
+hello();
+sup();
--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,65 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 
 module.exports = {
-    entry: './assets/js/index.js',
+    entry: {
+        app: './assets/index.js'
+    },
     output: {
-        filename: 'bundle.js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist')
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist'
     },
     module: {
         rules: [
-            //         {
-            // //         test: /\.js$/,
-            // //         exclude: /node_modules/,
-            // //         use: ['style-loader',
-            // //             "css-loader",
-            // //             "postcss-loader"
-            // //         ]
-            // //     },
             {
                 test: /\.css$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'postcss-loader'
-                ]
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader', 'postcss-loader']
+                })
+            },
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: [
+                        { loader: 'css-loader', options: { sourceMap: true } },
+                        { loader: 'postcss-loader', options: { sourceMap: true } },
+                        { loader: 'sass-loader', options: { sourceMap: true } }
+                    ]
+                })
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
             }
         ]
-
-    }    //     {
-    //         test: /\.scss$/,
-    //         use: [
-    //             {
-    //                 loader: 'style-loader'
-    //             },
-    //             { loader: 'css-loader' },
-    //             { loader: 'postcss-loader' },
-    //             { loader: 'sass-loader' },
-    //             {
-    //                 loader: 'css-loader',
-    //                 options: {
-    //                     sourceMap: true
-    //                 }
-    //             }
-    //         ]
-    //     }
-    //     ]
-    // }
+    },
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new ExtractTextPlugin('style.css'),
+        new HtmlWebPackPlugin({
+            filename: 'index.html',
+            template: 'assets/views/index.html',
+            chunks: ['app']
+        }),
+        new BrowserSyncPlugin(
+            {
+                host: 'localhost',
+                port: 3000,
+                proxy: 'http://localhost:8080'
+            },
+            {
+                reload: false
+            }
+        )
+    ]
 };
